f9e178dfc64545f21bc56784bbeb15bb33218e84
e-nk <enocklagat747@gmail.com>
2023-02-15 09:47:54 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
   # write code here
+  attr_accessor :length, :width, :height
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    if is_triangle?
+      sides = [length, width, height]
+      if sides.uniq.size == 1
+        :equilateral
+      elsif sides.uniq.size == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+
+  end
+  class TriangleError < StandardError
+  end
+  private
+  def is_triangle?
+    sorted = [length, width, height].sort
+    longest_side = sorted.pop
+    longest_side < sorted.sum
+  end
+
+  def triangle_type
+
+  end
+
+
 end
+
+
+
+
